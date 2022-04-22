8d2214ee3b81363244780f5c9f0f62a5b7bb71ec
galliFrancesco/Scuola <galifrancesco.2809@gmail.com>
2022-04-22 13:13:31 +0200

    Classe Risposta JSON

--- VerificaWEB2/src/verificaweb2/Risposta.java
+++ VerificaWEB2/src/verificaweb2/Risposta.java
@@ -1,15 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package verificaweb2;
 
 /**
  *
- * @author galli_francesco
+ * @author galliFrancesco
+ * 
+ * @brief Prende le cose dal JSON e le trasmuta in un oggetto per non impazzire 
  */
 public class Risposta {
     Boolean stato;
     String dato;
+    
+    public Risposta(){
+    
+    }
+    
+    // SET
+    public void setStato(boolean stato){
+        this.stato = stato; 
+    }  
+    public void setDato(){
+        this.dato = dato; 
+    }
+    // GET
+    public boolean getStato(){
+        return stato; 
+    }
+    public String getDato(){
+        return dato; 
+    }
 }
