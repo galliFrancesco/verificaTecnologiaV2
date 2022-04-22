dfc789763fe5df0c34f7106869c1607ace4b88c6
galliFrancesco/Scuola <galifrancesco.2809@gmail.com>
2022-04-22 13:20:41 +0200

    Classe Risposta JSON

--- VerificaWEB2/src/verificaweb2/Risposta.java
+++ VerificaWEB2/src/verificaweb2/Risposta.java
@@ -3,29 +3,36 @@ package verificaweb2;
 /**
  *
  * @author galliFrancesco
- * 
- * @brief Prende le cose dal JSON e le trasmuta in un oggetto per non impazzire 
+ *
+ * @brief Prende le cose dal JSON e le trasmuta in un oggetto per non impazzire
  */
 public class Risposta {
+
     Boolean stato;
     String dato;
-    
-    public Risposta(){
-    
+
+    public Risposta() {
+
     }
-    
+
     // SET
-    public void setStato(boolean stato){
-        this.stato = stato; 
-    }  
-    public void setDato(){
-        this.dato = dato; 
+    /**
+     * TRUE -> Fai le cose FALSE -> Non fare cose
+     */
+    public void setStato(boolean stato) {
+        this.stato = stato;
     }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+
     // GET
-    public boolean getStato(){
-        return stato; 
+    public boolean getStato() {
+        return stato;
     }
-    public String getDato(){
-        return dato; 
+
+    public String getDato() {
+        return dato;
     }
 }
--- VerificaWEB2/src/verificaweb2/WebService.java
+++ VerificaWEB2/src/verificaweb2/WebService.java
@@ -16,91 +16,119 @@ import org.json.JSONObject;
 /**
  *
  * @author galli_francesco
- * 
- * @brief Fa i metodi relativi al server 
- * 
+ *
+ * @brief Fa i metodi relativi al server
+ *
  */
 public class WebService {
-    
-    String host_cattedra = "http://172.16.102.100/"; 
-    
-    public WebService(){
-    
+
+    Risposta r;
+
+    String host_cattedra;
+
+    public WebService() {
+        host_cattedra = "http://172.16.102.100/";
     }
-    
-    
+
     /**
-     * EX: http://HOST_CATTEDRA/SaveStrings/register.php?username=aaa&password=bbb
+     * EX:
+     * http://HOST_CATTEDRA/SaveStrings/register.php?username=aaa&password=bbb
+     *
      * @brief registra l'utente "aaa" con password "bbb"
-     * 
+     *
      * @param username
      * @param password
-     * 
+     *
      * @throws java.net.MalformedURLException
      */
-    public String register(String username, String password) throws MalformedURLException, IOException{
-        
-        String URLBase = host_cattedra + "SaveStrings/register.php?username="; 
+    public String register(String username, String password) throws MalformedURLException, IOException {
+
+        String URLBase = host_cattedra + "SaveStrings/register.php?username=";
         String webRequest = URLBase + username + "&password=" + password; // url completo  
-        
-        URL request = new URL(webRequest); 
+
+        URL request = new URL(webRequest);
         String result = new BufferedReader(new InputStreamReader(request.openStream())).lines().collect(Collectors.joining("\n")); // manda la richiesta
-        
-        // JSON
+
+        // JSON <- Lo faccio qui, perchè ognuno ha una propria risposta
         String jsonString = result; //assign your JSON String here
         JSONObject obj = new JSONObject(jsonString);
-        
+
         String oggetto = obj.getString("status");
-        if(!oggetto.equals("ok")){
+        if (!oggetto.equals("ok")) {
             // allora non si può registrare
-            return obj.getString("message"); 
+            return obj.getString("message");
         } else {
             // si può registrare
             //return obj.get("Result").toString();
             return "Utente Registrato :D";
         }
-        
-        /** JSON FORMAT ? 
-         * {"STATUS":OK, "RESULT":"quello che ti serve"}
-         * 
+
+        /**
+         * JSON FORMAT ? {"STATUS":OK, "RESULT":"quello che ti serve"}
+         *
          * {"STATUS":ERROR, "MESSAGE":"bibo"}
-         */ 
+         */
     }
-    
+
     /**
-     * EX: http://HOST_CATTEDRA/SaveStrings/getToken.php?username=aaa&password=bbb
-     * @brief prende il token per l'utente registrato in quel modo 
-     * 
+     * EX:
+     * http://HOST_CATTEDRA/SaveStrings/getToken.php?username=aaa&password=bbb
+     *
+     * @brief prende il token per l'utente registrato in quel modo
+     *
      * @param username
-     * @param password 
-     * 
-     * @throws java.net.MalformedURLException 
+     * @param password
+     *
+     * @throws java.net.MalformedURLException
      */
-    /*
-    public Risposta getToken(String username, String password) throws MalformedURLException, IOException{
+    public Risposta getToken(String username, String password, Risposta r) throws MalformedURLException, IOException {
+
+        r = new Risposta();
         
-        String URLBase = "http://HOST_CATTEDRA/SaveStrings/register.php?username="; 
+        String URLBase = "http://HOST_CATTEDRA/SaveStrings/register.php?username=";
         String webRequest = URLBase + username + "&password=" + password; // url completo  
-        
-        URL request = new URL(webRequest); 
+
+        URL request = new URL(webRequest);
         String result = new BufferedReader(new InputStreamReader(request.openStream())).lines().collect(Collectors.joining("\n")); // manda la richiesta
         
-        //return result; // CHIEDERE SE ARRIVA COME JSON
-    }*/
-    
-    public void setString(){
-    
+        // {"status":"ok","result":{"token":"85456c36ea2b1f513cf5d708fa6d336d"}}
+        // {"status":"error","message":"username o password errati"}
+        
+        // JSON <- Lo faccio qui, perchè ognuno ha una propria risposta
+        String jsonString = result; //assign your JSON String here
+        JSONObject obj = new JSONObject(jsonString);
+        
+        
+        String oggetto = obj.getString("status");
+        if (!oggetto.equals("ok")) {
+            // allora non ritorna il token     
+            r.setStato(false);
+            r.setDato(obj.getString("message"));
+            
+            
+            
+        } else {
+            // Ritorna il token
+            //return obj.get("Result").toString();
+            //return "Utente Registrato :D";
+        }
+        
+        return r;
     }
-    
-    public void getString(){
-    
+
+    public void setString() {
+
     }
-    
-    public void deleteString(){
-    
+
+    public void getString() {
+
     }
-    
-    public void getKeys(){
-    
+
+    public void deleteString() {
+
+    }
+
+    public void getKeys() {
+
     }
 }
